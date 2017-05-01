cf20cfbdbc0f9c77a9914a99835ef7d97e2b8fa4
Ana Toma <ana.toma92@gmail.com>
2017-05-01 14:00:52 +0300

    Add files via upload

--- CoffeeBox/js/index.js
+++ CoffeeBox/js/index.js
@@ -179,5 +179,19 @@ var Modal = React.createClass({
   }
 });
 
+/*
+$(document).on('click', '.panel-heading span.clickable', function(e){
+    var $this = $(this);
+	if(!$this.hasClass('panel-collapsed')) {
+		$this.parents('.panel').find('.panel-body').slideUp();
+		$this.addClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+	} else {
+		$this.parents('.panel').find('.panel-body').slideDown();
+		$this.removeClass('panel-collapsed');
+		$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+	}
+})*/
+
 ReactDOM.render(React.createElement(Layout, null), document.getElementById("app"));
 //ReactDOM.render(<Modal />, document.getElementById("app"));
